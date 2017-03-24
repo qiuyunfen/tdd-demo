2c08e2e3d50afbafb01014a3501e90d7c710b429
qiuyunfen <1150382430@qq.com>
2017-03-24 15:14:48 +0800

    rename math

--- spec/math/mathSpec.js
+++ spec/math/mathSpec.js
@@ -1,6 +1,6 @@
 describe('math', function() {
     it('add two prands', function(){
-        var add = require('../../lib/m/math');
+        var add = require('../../lib/math/math');
         var sum = add(1,2);
         expect(sum).toEqual(3);      
     })
