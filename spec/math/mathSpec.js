f4b20ab0c2e39c9725664b47d33f05f2c09514d8
qiuyunfen <1150382430@qq.com>
2017-03-24 15:09:01 +0800

    implement math add

---
+++ spec/math/mathSpec.js
@@ -0,0 +1,9 @@
+describe('math', function() {
+    it('add two prands', function(){
+        var add = require('../../lib/m/math');
+        var sum = add(1,2);
+        expect(sum).toEqual(3);      
+    })
+
+    
+});
