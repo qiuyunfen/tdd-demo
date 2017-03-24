6be47b2f05b2f2309596a2ce72c4ff3f7feebe76
qiuyunfen <1150382430@qq.com>
2017-03-24 15:21:03 +0800

    implement minus method

--- spec/math/mathSpec.js
+++ spec/math/mathSpec.js
@@ -1,7 +1,12 @@
 describe('math', function() {
-    it('add two prands', function(){
-        var add = require('../../lib/math/math');
-        var sum = add(1,2);
+    var math = require('../../lib/math/math');
+    it('should return the sum of two oprands', function(){
+        var sum = math.add(1,2);
         expect(sum).toEqual(3);      
+    })
+
+    it('should return the substraction of two oprands', function() {
+        var substraction = math.minus(3,2);
+        expect(substraction).toEqual(1);
     })
 });
