828b6634651f18803dcf8c86dfceedb108e12235
qiuyunfen <1150382430@qq.com>
2017-03-24 16:25:19 +0800

    delete notice

--- spec/FizzBuzz/FizzBuzzSpec.js
+++ spec/FizzBuzz/FizzBuzzSpec.js
@@ -2,7 +2,6 @@ describe('FizzBuzz', function() {
     var FizzBuzz = require('../../lib/FizzBuzz/FizzBuzz'); 
     it('should return string number when number is not 3 mul nor 5 mul', function() {
         var str = FizzBuzz(1);
-        //expect(str).toEqual('1');
         expect(str === '1').toEqual(true);
     })
 
