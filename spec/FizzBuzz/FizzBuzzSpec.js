10ed0715322f8795162196d38842f705b94218a6
qiuyunfen <1150382430@qq.com>
2017-03-24 16:12:46 +0800

    implement one test

--- spec/FizzBuzz/FizzBuzzSpec.js
+++ spec/FizzBuzz/FizzBuzzSpec.js
@@ -1,5 +1,6 @@
 describe('FizzBuzz', function() {
-    it('should return string number when not 3 mul nor 5 mul', function() {
+    var FizzBuzz = require('../../lib/FizzBuzz/FizzBuzz'); 
+    it('should return string number when number is not 3 mul nor 5 mul', function() {
         var str = FizzBuzz(1);
         //expect(str).toEqual('1');
         expect(str === '1').toEqual(true);
